b9e33b6c7d653d232ff8c5ac780d7b7b344d09af
Rakesh Mistry <mistryrn@mcmaster.ca>
2017-01-23 07:49:43 -0500

    Styling, plus code cleanup/commenting

--- js/main.js
+++ js/main.js
@@ -4,18 +4,28 @@ $(function() {
     var orders = [];
     var deferred = $.Deferred();
 
+    function getOrders() {
+      return orders;
+    }
+
+    function addOrders(newOrders) {
+      orders = orders.concat(newOrders);
+    }
+
     function getAllOrders(pageNo) {
       if (pageNo === undefined) {
         pageNo = 1;
       }
       console.log("Checking page", pageNo);
 
+      // AJAX call to my own wrapper API, since this Shopify store doesn't play nice with cross-domain requests
       var endpoint = "https://wrapapi.com/use/mistryrn/shopicruit-orders/get/0.0.1?page=" + pageNo + "&wrapAPIKey=8Z3OIH8MsAAfO5hwuvUHv31GgummtWs8";
       $.getJSON(endpoint, function(response) {
+        // make GET requests until we've got all the orders
         if (response.success) {
           console.log("Successfully got orders from page", pageNo);
           updateTotal(response.data.orders);
-          appendOrders(response.data.orders);
+          addOrders(response.data.orders);
           getAllOrders(pageNo + 1);
         } else {
           console.log("No orders left as of page", pageNo);
@@ -36,30 +46,20 @@ $(function() {
       }
     }
 
-    function setOrders(newOrders) {
-      orders = newOrders;
-    }
-
-    function getOrders() {
-      return orders;
-    }
-
-    function appendOrders(newOrders) {
-      orders = orders.concat(newOrders);
-    }
-
     $.when(getAllOrders()).then(function(orders) {
-      $("#status").text("Your total is:");
+      $("#status").text("Your total order revenue is:");
       console.log("Total number of orders:", orders.length);
     });
 
 });
 
+// formats numbers to money strings
 function formatMoney(number) {
   // regex from: http://stackoverflow.com/a/14428340/2763340
   return number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 }
 
+// converts money strings to numbers
 function getNumber(str) {
   // regex from http://stackoverflow.com/a/559178/2763340
   return Number(str.replace(/[^0-9\.]+/g,""));
