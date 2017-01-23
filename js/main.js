f17f5df559ce09cef1e7f6dafa748dcb6a84d54c
Rakesh Mistry <mistryrn@mcmaster.ca>
2017-01-23 05:42:03 -0500

    Slight refactorization of js; added some getters and setters for readability, promisify the GET request

--- js/main.js
+++ js/main.js
@@ -1,48 +1,34 @@
 $(function() {
-    console.log("I'm ready!");
+    console.log("jQuery ready!");
 
-    // var endpoint = "https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6";
     var orders = [];
+    var deferred = $.Deferred();
 
-    function getOrders(pageNo) {
-      var endpoint = "https://wrapapi.com/use/mistryrn/shopicruit-orders/get/0.0.1?page=" + pageNo + "&wrapAPIKey=8Z3OIH8MsAAfO5hwuvUHv31GgummtWs8";
-      $.getJSON(endpoint, function(response) {
-        if (response.success) {
-          console.log("success from page", pageNo);
-          return response.data.orders;
-        } else {
-          console.log("zero orders on page", pageNo);
-          return null;
-        }
-      });
-    }
-
-    function getAllOrders(pageNo, orders) {
+    function getAllOrders(pageNo) {
       if (pageNo === undefined) {
         pageNo = 1;
       }
-      if (orders === undefined) {
-        orders = [];
-      }
       console.log("Checking page", pageNo);
+
       var endpoint = "https://wrapapi.com/use/mistryrn/shopicruit-orders/get/0.0.1?page=" + pageNo + "&wrapAPIKey=8Z3OIH8MsAAfO5hwuvUHv31GgummtWs8";
       $.getJSON(endpoint, function(response) {
         if (response.success) {
-          console.log("success from page", pageNo);
-          $("#status").text("Calculating your total...");
-          orders = orders.concat(response.data.orders);
+          console.log("Successfully got orders from page", pageNo);
           updateTotal(response.data.orders);
-          getAllOrders(pageNo+1, orders);
+          appendOrders(response.data.orders);
+          getAllOrders(pageNo + 1);
         } else {
-          console.log("zero orders on page", pageNo);
-          $("#status").text("Your total is:");
-          setOrders(orders);
+          console.log("No orders left as of page", pageNo);
+          deferred.resolve(getOrders());
         }
       });
+
+      return deferred.promise();
     }
 
     function updateTotal(orders) {
       var total = getNumber($("#total").text());
+
       for (var i = 0; i < orders.length; i++) {
         var orderCost = +orders[i].total_price;
         total += orderCost;
@@ -50,20 +36,31 @@ $(function() {
       }
     }
 
-    function formatMoney(number) {
-      // regex from: http://stackoverflow.com/a/14428340/2763340
-      return number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    function setOrders(newOrders) {
+      orders = newOrders;
     }
 
-    function getNumber(str) {
-      // regex from http://stackoverflow.com/a/559178/2763340
-      return Number(str.replace(/[^0-9\.]+/g,""));
+    function getOrders() {
+      return orders;
     }
 
-    function setOrders(orders) {
-      orders = orders;
+    function appendOrders(newOrders) {
+      orders = orders.concat(newOrders);
     }
 
-    getAllOrders();
+    $.when(getAllOrders()).then(function(orders) {
+      $("#status").text("Your total is:");
+      console.log("Total number of orders:", orders.length);
+    });
 
 });
+
+function formatMoney(number) {
+  // regex from: http://stackoverflow.com/a/14428340/2763340
+  return number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+}
+
+function getNumber(str) {
+  // regex from http://stackoverflow.com/a/559178/2763340
+  return Number(str.replace(/[^0-9\.]+/g,""));
+}
